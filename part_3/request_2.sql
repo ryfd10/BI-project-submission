d99d9aa073c4c7aa22cbcb5ed9414ecfc095f435
L.Feld <tichnut15@8547900.org.il>
2026-01-04 19:29:47 +0200

    Create request_2.sql

---
+++ part_3/request_2.sql
@@ -0,0 +1,13 @@
+DECLARE @X INT = 32;
+
+SELECT
+    Numbers1.val AS val1,
+    Numbers2.val AS val2,
+    Numbers3.val AS val3
+FROM Numbers AS Numbers1
+JOIN Numbers AS Numbers2 ON Numbers1.val > Numbers2.val
+JOIN Numbers AS Numbers3 ON Numbers1.val > Numbers3.val
+                         AND Numbers2.val > Numbers3.val
+WHERE Numbers1.val + Numbers2.val + Numbers3.val = @X;
+
+
